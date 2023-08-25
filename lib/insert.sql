4ae460ba644f011de2d02f5ac8c5c0a8ff7cc4a5
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-08-25 01:53:46 -0400

    Lab Completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'F', 'white', 'mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6,'M','red', 'nice', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'F','brown', 'rude', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 8, 'M', 'white', 'kind', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'F', 'red', 'thoughtful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 10, 'M', 'black','timid', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 11, 'F', 'purple', 'wise', 1);
